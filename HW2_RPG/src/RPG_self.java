babd20fdbc1f0b438b0419f458798d038bdb3c70
willychen0146 <willychen0146@gmail.com>
2024-03-01 01:51:27 +0800

    chore: update HW2_ver2

--- HW2_RPG/src/RPG_self.java
+++ HW2_RPG/src/RPG_self.java
@@ -1,63 +1,8 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Arrays;
-
-import com.google.gson.*;
-
-class OutputFormat{
-    int[] defence;
-    int[] attack;
-    int k;
-    int answer;
-}
-
-class test_RPG{
-    public static void main(String[] args)
-    {
-        Gson gson = new Gson();
-        OutputFormat[] datas;
-        int num_ac = 0;
-        int user_ans;
-        OutputFormat data;
-
-        try {
-            datas = gson.fromJson(new FileReader(args[0]), OutputFormat[].class);
-            for(int i = 0; i<datas.length;++i)
-            {
-                data = datas[i];
-                user_ans = new RPG(data.defence, data.attack).maxDamage(data.k);
-                System.out.print("Sample"+i+": ");
-                if(data.answer == user_ans)
-                {
-                    System.out.println("AC");
-                    num_ac++;
-                }
-                else
-                {
-                    System.out.println("WA");
-                    System.out.println("Data_atk:  " + Arrays.toString(data.attack));
-                    System.out.println("Data_dfc:  " + Arrays.toString(data.defence));
-                    System.out.println("Test_ans:  " + data.answer);
-                    System.out.println("User_ans:  " + user_ans);
-                    System.out.println("");
-                }
-            }
-            System.out.println("Score: "+num_ac+"/"+datas.length);
-        } catch (JsonSyntaxException e) {
-            e.printStackTrace();
-        } catch (JsonIOException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-}
-// paste your own RPG class here :)
-class RPG {
+class RPG_self {
     private int[] defense;
     private int[] attack;
 
-    public RPG(int[] defence, int[] attack){
+    public RPG_self(int[] defence, int[] attack){
         // Initialize some variables
         this.defense = defence;
         this.attack = attack;
@@ -111,7 +56,7 @@ class RPG {
         return maxDamage[n];
     }
     public static void main(String[] args) {
-        RPG sol = new RPG(new int []{5,4,1,7,98,2},new int []{200,200,200,200,200,200});
+        RPG_self sol = new RPG_self(new int []{5,4,1,7,98,2},new int []{200,200,200,200,200,200});
         System.out.println(sol.maxDamage(6));
         //1: boost, 2: attack, 3: boost, 4: attack, 5: boost, 6: attack
         //maxDamage: 1187
